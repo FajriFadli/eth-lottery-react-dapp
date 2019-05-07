2cb72a796a0f5fc361a95e18f5b9c9cd7ba46c94
Fajri Fadli <fajri.fadli104@gmail.com>
2019-05-07 18:03:23 +0700

    Updates

---
+++ precache-manifest.23a37ae3ca7cbcba720ad2a6963b0e62.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "319389f8287889e8500c",
+    "url": "/eth-lottery-react-dapp/static/js/runtime~main.f5047275.js"
+  },
+  {
+    "revision": "98dc1705c65badfda28b",
+    "url": "/eth-lottery-react-dapp/static/js/main.a689a239.chunk.js"
+  },
+  {
+    "revision": "754838f41e65beb8a5b8",
+    "url": "/eth-lottery-react-dapp/static/js/2.63880568.chunk.js"
+  },
+  {
+    "revision": "98dc1705c65badfda28b",
+    "url": "/eth-lottery-react-dapp/static/css/main.b0c90f60.chunk.css"
+  },
+  {
+    "revision": "7dfce785e88390fe119a816c8c69899c",
+    "url": "/eth-lottery-react-dapp/index.html"
+  }
+];
